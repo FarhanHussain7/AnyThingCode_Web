2560976f2ddb87b5fd51336db47cca5ca5e3ba9f
Farhanhussain7 <farhanhussain81303@gmail.com>
2025-08-27 23:10:45 +0530

    First files of project

---
+++ client/header.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+  <img src="./public/logo3.png" alt="" class="img-fluid" style="max-height: 40px;">
+</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Catogrey</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Latest Question</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
